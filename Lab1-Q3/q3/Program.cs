26cab9e726f30625f2fdfd5abe105f9b8c4435a4
rdchkulasinghe <140293329+rdchkulasinghe@users.noreply.github.com>
2024-03-09 12:48:25 +0530

    Add files via upload

---
+++ Lab1-Q3/q3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum=0;
+            if (number > 0)
+            {
+                for (int i = 0; i <= number; i++)
+                {
+                    sum = sum + i;
+                }
+                Console.WriteLine(sum);
+            }
+            else 
+            { 
+                Console.WriteLine("Error"); 
+            }
+        }
+    }
+}
