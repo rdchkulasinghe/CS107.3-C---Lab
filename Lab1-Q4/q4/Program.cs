0dd642bce62e8f7d707b469a29d9c222e2efc670
rdchkulasinghe <140293329+rdchkulasinghe@users.noreply.github.com>
2024-03-19 14:20:22 +0530

    Add files via upload

---
+++ Lab1-Q4/q4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= number; i++)
+            {
+                int result = Fibo(i);
+                Console.WriteLine(result);
+            }
+        }
+        static int Fibo (int i)
+        {
+            if(i <= 1)
+            {
+                return i;
+            }
+            else
+            {
+                return Fibo(i - 1)+Fibo(i - 2);
+            }
+  
+        }
+    }
+}
---
+++ Lab1-Q5/q5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<12;i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + number * i);
+            }
+        }
+    }
+}
